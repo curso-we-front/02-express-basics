993d55aaf427d3e4e144dcff78765b391277c605
jaimeveloso <jaimeveloso.cond@gmail.com>
2026-05-23 01:16:59 +0200

    [Added] Routes and middleware

--- src/routes/articles.js
+++ src/routes/articles.js
@@ -1,25 +1,43 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
+const express = require("express")
+const path = require("path")
+const fs = require("fs")
 
-const router = express.Router();
-const DATA_PATH = path.join(__dirname, '../../data/articles.json');
+const router = express.Router()
+const DATA_PATH = path.join(__dirname, "../../data/articles.json")
+console.log(DATA_PATH)
 
 // Helper para leer artículos
 function getArticles() {
-  const raw = fs.readFileSync(DATA_PATH, 'utf-8');
-  return JSON.parse(raw);
+  const raw = fs.readFileSync(DATA_PATH, "utf-8")
+  return JSON.parse(raw)
 }
 
 // TODO: GET /articles — devuelve solo los publicados
-router.get('/articles', (req, res, next) => {
+router.get("/articles", (req, res, next) => {
+  const articles = getArticles()
+  const articlesPublished = articles.filter((article) => article.published)
+  res.status(200).json(articlesPublished)
   // implementar
-});
+})
 
 // TODO: GET /articles/:id — devuelve el artículo por id si está publicado
-router.get('/articles/:id', (req, res, next) => {
+router.get("/articles/:id", (req, res, next) => {
+  const articles = getArticles()
+  const id = Number(req.params.id)
+  const idArticlesPublished = articles.find(
+    (article) => article.published && article.id === id,
+  )
+
+  if (!idArticlesPublished) {
+    const error = new Error(
+      "Artículo no publicado, por favor, inténtelo de nuevo",
+    )
+    error.status = 404
+    return next(error)
+  }
+  res.status(200).json(idArticlesPublished)
   // implementar
   // Usa next(error) para pasar errores al manejador central
-});
+})
 
-module.exports = router;
+module.exports = router
--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
-const app = require('./app');
-
-const PORT = process.env.PORT || 3000;
+const app = require("./app")
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
-});
+  console.log(`Servidor escuchando en http://localhost:${PORT}`)
+})
